b62a5ed0a2cdf095aed1557818e85551450314a6
AbianGustavo <abiancma12@gmail.com>
2024-01-22 15:33:02 +0000

    Made some fixes on urls and templates

--- payment/urls.py
+++ payment/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('done/', views.payment_done, name='done'),
     path('proccess/', views.payment_proccess, name='proccess'),
     path('<int:payment_id>/pdf/', views.payment_pdf, name='pdf'),
+    path('<int:payment_id>/csv/', views.payment_csv, name='csv'),
 ]
--- transaction/urls.py
+++ transaction/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('done/', views.transaction_done, name='done'),
     path('outgoing/', views.transaction_outgoing_proccess, name='outgoing'),
     path('<int:transaction_id>/pdf/', views.transaction_pdf, name='pdf'),
+    path('<int:transaction_id>/csv/', views.transaction_csv, name='csv'),
+    path('csv/', views.all_transaction_csv, name='all_csv'),
 ]
